4d6e7e241e01d3eaceba04692eb8c900f8896f9b
寫詩羊 <Habuche@gmail.com>
2021-09-23 12:52:02 +0800

    feat: define types.

--- type.d.ts
+++ type.d.ts
@@ -1,12 +1,44 @@
-export interface RollCall {
-  type: 'rollcall',
+type StudentStatus = 'join' | 'dropout';
+
+export interface Record<T, D = {}> {
+  type: T,
+  data?: D,
   date: Date,
 }
 
-export interface Deposit {
-  type: 'deposit',
+export type StudentRollCall = Record<'rollcall'>
+
+export type StudentDeposit = Record<'deposit', {
   name: string,
   money: number,
   times: number,
+}>
+
+export type StudentRecord = (StudentDeposit | StudentRollCall)[];
+
+export interface GetStudentInfoRes {
+  name: string,
+  status: StudentStatus,
+  picture: string,
+  createdAt: Date,
+  updatedAt: Date,
+  records: StudentRecord
+}
+
+interface RollCallReq {
   date: Date,
+  studentIdList: number[]
+}
+
+interface Req<T> {
+  data: T
+}
+
+export interface Student {
+  id: number,
+  teacherId: number,
+  name: string,
+  status: StudentStatus,
+  lineId: string,
+  picture: string,
 }
